65e35318f1b87e72c3c729d7b746608ae6e01085
SovietWeirdo <104281342+SovietWeirdo@users.noreply.github.com>
2023-05-13 10:15:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,19 @@ videocard.frame(DISPLAYSURF)
 programCounter = 32767
 while True:
   time.sleep(0)
-  #for event in pygame.event.get():
-    #if event.type == pygame.QUIT:
-      #pygame.quit()
-      #sys.exit()
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
   pygame.display.update()
   programCounter += 1
   programAddress = hex(programCounter)
   programAddress = programAddress[2:6]
   programAddress = programAddress.upper()
-  #programAddress = instructionHandler.handle(programAddress)
-  #programCounter = int(programAddress,16)
+  programAddress = instructionHandler.handle(programAddress)
+  programCounter = int(programAddress,16)
   if programAddress == 'FFFF':
     sys.exit("Tried to access memory out of the 0000-FFFF range")
   print(programAddress)
-  videocard.loadScreen(DISPLAYSURF)
+  #videocard.loadScreen(DISPLAYSURF)
+  videocard.frame(DISPLAYSURF)
